e1b1ef5022756f95a8b437c33b11667956260ad3
Nika Korchok <nikakw@gmail.com>
2017-12-15 06:27:44 -0500

    updated pixels manipulation js file

--- assets/js/pixels.js
+++ assets/js/pixels.js
@@ -7,28 +7,70 @@ function centerCanvas() {
 	cnv.position(x,y);
 }
 */
+var points = [];
+var img;
 
 function setup() {
 	var canvas = createCanvas(500,500); 
-	canvas.parent('sketch1');
-  	background(0); 
-  	noStroke(); 
-
-  	var gridSize = 50;
-
-  	for (var x = gridSize; x <= width - gridSize; x += gridSize) {
-    	for (var y = gridSize; y <= height - gridSize; y += gridSize) {
-      	noStroke();
-      	fill(255);
-      	rect(x-1, y-1, 3, 3);
-      	stroke(255, 50);
-      	line(x, y, width/2, height/2);
-    	}
-  	}
+  canvas.parent('sketch1');
+  img = loadImage("/assets/images/163images/nikaInitialImage.jpg");
+  background(0); 
+  noStroke(); 
+    //for each point, call edge.draw to draw edges connecting all consecutive points
 }
 
+function convolution(x, y, matrixsize, img){
+  var brightnessavg = 0.0;
+  var offset = matrixsize/2;
+  for (var i=0; i<matrixsize; i++){
+    for(var j=0; j<matrixsize; j++){
+      var xloc = (x+i) - offset;
+      var yloc = (y-j) - offset;
+      var loc = xloc + img.width*yloc;
+      var mid = x + y*img.width;
+      loc = constrain(loc, 0, img.pixels.length-1);
+      brightnessavg += abs((brightness(img.pixels[mid]))- ((brightness(img.pixels[loc]))));
+      //print(brightnessavg);    
+    }
+  }
+  brightnessavg /= 9.00;
+  brightnessavg = constrain(brightnessavg,0,255);
+}
+
+
+
 function draw() {
+  img.loadPixels();
+  var height = img.height;
+  var width = img.width;
+  var diff = 0.0;
+  var matrixsize = 3;
+  for (var x=0; x < width; x++){
+    for (var y=0; y < height; y++){
+      //int pix = x + y*img.width;
+      diff = convolution(x,y,matrixsize,img);
+      if ((diff > 25) && (diff < 40)) {
+        points.push(new Point(x,y));
+      }
+    }
+  }
+  updatePixels();
+  for(var i=0; i < points.length; i++){
+    points[i].display();
+  }
   // put drawing code here
+}
+
+
+ function Point(x, y){
+   this.x = x;
+   this.y = y;
+  
+    this.display() = function() {
+      noStroke();
+      fill(0,0,0);
+      triangle(x, y, x+5, y+5, x+5, y);
+    }
 }
 
 /*
